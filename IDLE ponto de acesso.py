06c1eff7476a081ecc03752c2930f1ec3b968748
DILMA-CACHIO <123216580+DILMA-CACHIO@users.noreply.github.com>
2023-07-13 12:44:56 +0100

    Add files via upload

---
+++ IDLE ponto de acesso.py
@@ -0,0 +1,104 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\DR-DILMA\Downloads\ponto_de_acesso_electronico.py
+
+
+»»»»»»»»»»»»»»»»»»»» CODE ACADEMY:GIRLS ««««««««««««««««««
+
+Sistema de registro de presenças
+
+
+
+
+Menu de opções:
+
+[1] - Inserir nova aluna
+[2] - Registrar presença
+[3] - Visualizar alunas
+[9] - Sair
+
+
+Escolha uma das opções do menu: 1
+Digite o seu nome: Dilma Cachio
+Digite o seu B.I: 004658318LA040
+Digite o seu curso: PYTHON
+Digite o seu computador: PERSONAL PC-MARCA ITEL
+Aluna registrada com sucesso.
+
+
+Menu de opções:
+
+[1] - Inserir nova aluna
+[2] - Registrar presença
+[3] - Visualizar alunas
+[9] - Sair
+
+
+Escolha uma das opções do menu: 3
+1º - Alcina Eurico
+2º - Deura Fernandes
+3º - Deisy Costa
+4º - Domingas Queta
+5º - Elisa Mavinga
+6º - Fernanda Zacarias
+7º - Geunila Gomes
+8º - Lauriane Vunge
+9º - Isabel Pascoal
+10º - Joice Calunga
+11º - Dilma Cachio
+
+
+Menu de opções:
+
+[1] - Inserir nova aluna
+[2] - Registrar presença
+[3] - Visualizar alunas
+[9] - Sair
+
+
+Escolha uma das opções do menu: 2
+Digite o número de seu B.I: 004658318LA040
+Digite a hora de entrada: 10 HORAS
+
+
+Menu de opções:
+
+[1] - Inserir nova aluna
+[2] - Registrar presença
+[3] - Visualizar alunas
+[9] - Sair
+
+
+Escolha uma das opções do menu: 2
+Digite o número de seu B.I: 004658318LA040
+Digite a hora de saída: 12.45
+
+
+Menu de opções:
+
+[1] - Inserir nova aluna
+[2] - Registrar presença
+[3] - Visualizar alunas
+[9] - Sair
+
+
+Escolha uma das opções do menu: 2
+Digite o número de seu B.I: 004658318LA040
+A presença já foi registrada.
+
+
+Menu de opções:
+
+[1] - Inserir nova aluna
+[2] - Registrar presença
+[3] - Visualizar alunas
+[9] - Sair
+
+
+Escolha uma das opções do menu: 9
+
+
+Sistema finalizando...
+
+
